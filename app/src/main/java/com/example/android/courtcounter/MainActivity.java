ea7d29e278e1c33c0b3c0ff71d79fcf317e0ae77
Hudson Pereira <hudson.byte@gmail.com>
2018-05-06 12:26:27 -0300

    adding behavior for buttons

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayForTeamA(10);
     }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void plus3ForTeamA(View view) {
+       displayForTeamA(3);
+    }
+
+    public void plus2ForTeamA(View view) {
+        displayForTeamA(2);
+    }
+
+    public void freeThrowForTeamA(View view) {
+        displayForTeamA(1);
+    }
+
+
 }
